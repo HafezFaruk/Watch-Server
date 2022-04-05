c2ec767a5bed31ea83b2fb136908f67381fcd401
HafezFaruk <hmomarfaruk1454@gmail.com>
2022-04-05 17:43:57 +0600

    mongodb data added

--- index.js
+++ index.js
@@ -21,17 +21,11 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.nsljh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-// const client = new MongoClient(uri, {
-//   useNewUrlParser: true,
-//   useUnifiedTopology: true,
-// });
 
 async function run() {
   try {
     await client.connect();
-    const database = client.db("Watch");
+    const database = client.db("watch");
     const productCollection = database.collection("products");
     const orderCollection = database.collection("orders");
     const userCollection = database.collection("users");
